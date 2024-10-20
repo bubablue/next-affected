43209af7ffdce183ce7c76e117d6e398e70f574a
Jose <bubablue00@gmail.com>
2024-10-20 20:44:49 +0200

    update: prompt version

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const program = new Command();
 program
   .name("next-affected")
   .description("List Next.js pages affected by changes")
-  .version("0.0.2");
+  .version("0.0.3");
 
 program
   .command("init")
