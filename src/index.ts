0209704eb2872b230ce404827e3a4cdf2e79746e
Jose <bubablue00@gmail.com>
2024-12-30 21:05:14 +0100

    update: dependencies

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const program = new Command();
 program
   .name("next-affected")
   .description("List Next.js pages affected by changes")
-  .version("1.2.0");
+  .version("1.2.1");
 
 program
   .command("init")
