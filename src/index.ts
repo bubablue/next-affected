02b8c2ee149fcb3e349c7f9200c356164b533621
Jose <bubablue00@gmail.com>
2026-03-18 17:40:22 +0100

    update: deps

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const program = new Command();
 program
   .name("next-affected")
   .description("List Next.js pages affected by changes")
-  .version("1.2.2");
+  .version("1.2.3");
 
 program
   .command("init")
